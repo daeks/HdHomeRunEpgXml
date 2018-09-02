c9e2f12530639003a60173267d3eff9b68615273
Winterleaf <VGee@winterleafentertainment.com>
2018-09-02 15:05:30 -0400

    removed broken code

--- Util/JsonCalls.cs
+++ Util/JsonCalls.cs
@@ -73,14 +73,7 @@ namespace HdHomeRunEpgXml.Util
             }
         }
 
-        public static List<HdConnectLineup> GetHdConnectLineUp(this HdConnectDiscover discover)
-        {
-            var uri = new Uri(discover.LineupURL);
-            var hc = new HttpClient();
-            var result = hc.GetByteArrayAsync(uri).Result;
-            string json = Encoding.UTF8.GetString(result);
-            return JsonConvert.DeserializeObject<List<HdConnectLineup>>(json);
-        }
+      
 
         public static HdConnectDiscover GetHdConnectDiscover(this HdConnectDevice device)
         {
